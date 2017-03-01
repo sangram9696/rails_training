c8a4f7b0d08a43057b298bd7e9b742774891843b
Sangram <sangram.desai@wgblindia.com>
2017-03-01 13:46:47 +0530

    toy_app done

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  validates :content,presence:true
+  validates :user_id,presence:true
 end
--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name,length: { maximum: 160 }, presence: true
+  validates :email, length: { maximum: 64 },presence: true
+  validates :address,length: { maximum: 256 }, presence: true
 end
