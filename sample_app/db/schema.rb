9cb154fac02f5f84a5656e5b2708ceb47d59a365
Sangram <sangram.desai@wgblindia.com>
2017-03-02 19:16:25 +0530

    user list done

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302105122) do
+ActiveRecord::Schema.define(version: 20170302132055) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170302105122) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end
