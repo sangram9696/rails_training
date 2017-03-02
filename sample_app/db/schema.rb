d2ac64be4d0c8a806d4db232aa5ae384de0309d8
Sangram <sangram.desai@wgblindia.com>
2017-03-02 12:12:51 +0530

    Minimum password standards

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302063616) do
+ActiveRecord::Schema.define(version: 20170302064011) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
