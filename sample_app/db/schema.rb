f01617a6eec481636becf3fb28f8eb01f25712b5
Sangram <sangram.desai@wgblindia.com>
2017-03-03 10:50:49 +0530

    account activation done

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302132055) do
+ActiveRecord::Schema.define(version: 20170303042238) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "address"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
 end
